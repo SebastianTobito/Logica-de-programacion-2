ba6054360874d993d185a1f1e2f5689689e3b293
sebastian tobito saavedra <sebastiantobito@gmail.com>
2024-01-28 11:32:36 -0500

    en app.js borramos las variables y lo cambiamos por una función que recorta el código y hace la misma función

--- Desafio1/js-curso-2-main/app.js
+++ Desafio1/js-curso-2-main/app.js
@@ -1,10 +1,5 @@
 //1. Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
 
-//3. Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. 
-//Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
-//4. Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
-//5. Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.
-
 let titulo = document.querySelector('h1');
 titulo.innerHTML = "Hora del Desafío";
 
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del número secreto";
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un número del 1 al 10"; 
 
 function intentoUsuario(){
  alert('Click desde el boton')
 };
+
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+};
+asignarTextoElemento('h1', "Juego del número secreto");
+asignarTextoElemento('p', "Indica un número del 1 al 10");
