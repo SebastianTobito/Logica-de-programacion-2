d8b8fbd080dc7949be2af4dbbf4eb9f529147ddc
sebastian tobito saavedra <sebastiantobito@gmail.com>
2024-01-30 11:05:49 -0500

    agregamos al archivo app.js un if en la función verificarIntento para aconsejar al usuario si el número secreto es mayor o menor que el número que intentó

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 let numeroSecreto = generadorNumeroSecreto();
 
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+}
+asignarTextoElemento("h1", "Juego del número secreto");
+asignarTextoElemento("p", "Indica un número del 1 al 10");
 
+function generadorNumeroSecreto() {
+  return parseInt(Math.floor(Math.random() * 10) + 1);
+}
 
-function verificarIntento(){
- let numeroDelUsuario = parseInt(document.getElementById('valorUsuario').value);
- console.log("El número del usuario es de tipo: " + typeof(numeroDelUsuario));
- console.log("El número secreto es el: " + numeroSecreto);
- console.log("El número secreto es de tipo: " + typeof(numeroSecreto));
- console.log("La comparación entre el número secreto y el número del usuario es: " + numeroSecreto === numeroDelUsuario);
-};
-
-function asignarTextoElemento (elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-};
-asignarTextoElemento('h1', "Juego del número secreto");
-asignarTextoElemento('p', "Indica un número del 1 al 10");
- 
- function generadorNumeroSecreto(){
-  return parseInt(Math.floor(Math.random()*10) +1);;
- };
+function verificarIntento() {
+  let numeroDelUsuario = parseInt(
+    document.getElementById("valorUsuario").value
+  );
+  if (numeroDelUsuario === numeroSecreto) {
+    asignarTextoElemento("p", "Felicidades, acertaste!!");
+  } else {
+    if (numeroDelUsuario > numeroSecreto) {
+      asignarTextoElemento("p", "El número secreto es menor");
+    } else {
+      asignarTextoElemento("p", "El número secreto es mayor");
+    }
+  }
+  return;
+}
